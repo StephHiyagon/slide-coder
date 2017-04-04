7852b7e2cdfbfbf7e8b2a26b2f77ec3fe6b5e4a7
stephHiyagon <mv.hiyagon@gmail.com>
2017-04-04 11:52:03 -0500

    mejorando css y app

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,18 +5,22 @@ var envia=document.getElementById('js_envia');
 
 function enviando(event){
     var imprime=document.getElementById('js_imprimir');
-    var parrafo=document.createElement('p');
+    var parrafo1=document.createElement('p');
+    var parrafo2=document.createElement('p');
     var titulo=document.getElementById('js_title').value;
     var textarea=document.getElementById('js_post').value;
     var titulo_text=document.createElement("h3");
     var title_text=document.createTextNode(titulo);
-    var post_cont=document.createElement("textarea");
+    var post_cont=document.createElement("div");
     var post_text=document.createTextNode(textarea);
+    parrafo1.id="css_title";
+    parrafo2.id="css_post";
     titulo_text.appendChild(title_text);
     post_cont.appendChild(post_text);
-    parrafo.appendChild(titulo_text);
-    parrafo.appendChild(post_text);
-    imprime.appendChild(parrafo);
+    parrafo1.appendChild(titulo_text);
+    parrafo2.appendChild(post_text);
+    imprime.appendChild(parrafo1);
+    imprime.appendChild(parrafo2);
   }
 
 envia.addEventListener('click', enviando);
